c690b3182cf869ff1778cf7742c06875ebb0b3da
Denis Toledo <detoledo.denis@gmail.com>
2026-05-18 21:37:57 +0200

    add splash screen

--- app.config.ts
+++ app.config.ts
@@ -50,9 +50,9 @@ const config: ExpoConfig = {
         image: './assets/images/splash-icon.png',
         imageWidth: 200,
         resizeMode: 'contain',
-        backgroundColor: '#ffffff',
+        backgroundColor: '#22C55E',
         dark: {
-          backgroundColor: '#000000',
+          backgroundColor: '#22C55E',
         },
       },
     ],
