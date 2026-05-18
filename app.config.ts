210ecbf2bb375c3201b476c90d2d0e0a1b6c2c2a
Denis Toledo <detoledo.denis@gmail.com>
2026-05-18 21:25:09 +0200

    add app icons

--- app.config.ts
+++ app.config.ts
@@ -3,14 +3,16 @@ import type { ExpoConfig } from 'expo/config';
 const IS_DEV = process.env.APP_VARIANT === 'development';
 
 const config: ExpoConfig = {
-  name: IS_DEV ? 'Finance42.dev' : 'Finance42',
+  name: 'Finance42',
   slug: 'contas',
   version: '1.0.0',
   runtimeVersion: {
     policy: 'fingerprint',
   },
   orientation: 'portrait',
-  icon: './assets/images/icon.png',
+  icon: IS_DEV
+    ? './assets/images/app-icon-dev/ios/AppIcon~ios-marketing.png'
+    : './assets/images/app-icon/ios/AppIcon~ios-marketing.png',
   scheme: 'contas',
   userInterfaceStyle: 'automatic',
   ios: {
@@ -23,16 +25,22 @@ const config: ExpoConfig = {
   android: {
     adaptiveIcon: {
       backgroundColor: '#E6F4FE',
-      foregroundImage: './assets/images/android-icon-foreground.png',
-      backgroundImage: './assets/images/android-icon-background.png',
-      monochromeImage: './assets/images/android-icon-monochrome.png',
+      foregroundImage: IS_DEV
+        ? './assets/images/app-icon-dev/android/res/mipmap-xxxhdpi/ic_launcher_foreground.png'
+        : './assets/images/app-icon/android/res/mipmap-xxxhdpi/ic_launcher_foreground.png',
+      backgroundImage: IS_DEV
+        ? './assets/images/app-icon-dev/android/res/mipmap-xxxhdpi/ic_launcher_background.png'
+        : './assets/images/app-icon/android/res/mipmap-xxxhdpi/ic_launcher_background.png',
+      monochromeImage: IS_DEV
+        ? './assets/images/app-icon-dev/android/res/mipmap-xxxhdpi/ic_launcher_monochrome.png'
+        : './assets/images/app-icon/android/res/mipmap-xxxhdpi/ic_launcher_monochrome.png',
     },
     predictiveBackGestureEnabled: false,
     package: IS_DEV ? 'com.dnstld.contas.dev' : 'com.dnstld.contas',
   },
   web: {
     output: 'static',
-    favicon: './assets/images/favicon.png',
+    favicon: './assets/images/app-icon/web/favicon.ico',
   },
   plugins: [
     'expo-router',
